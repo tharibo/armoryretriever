d5ef39e4ebed3beba5c101813ad551bf073016ae
tharibo <tharibo@03c259dc-e102-11dd-9759-139c9ab14fc6>
2009-01-13 14:10:11 +0000

    Added proxy settings

--- trunk/ArmoryRetriever.py
+++ trunk/ArmoryRetriever.py
@@ -3,33 +3,58 @@
 """Retrieve your WoW character on the Armory"""
 
 import sys
-import httplib
+import urllib2
 from PyQt4 import QtCore, QtGui, uic
 
 
-def retrieveCharacterPage( pCharacterName, pRealmName, pContinentName ):
+def retrieveCharacterPage( pCharacterName, pRealmName, pContinentName, pProxyInfo ):
 	"""Opens a connection to the armory and retrieve the given character page"""
+
+	# Proxy info ?
+	if len( pProxyInfo ) > 0 :
+		# build a new opener that uses a proxy requiring authorization
+		proxySupport = urllib2.ProxyHandler({"http" : "http://%(user)s:%(pass)s@%(host)s:%(port)d" % pProxyInfo})
+		opener = urllib2.build_opener(proxySupport, urllib2.HTTPHandler)
+		# install opener
+		urllib2.install_opener(opener)
+
+	# Check zone
 	prefix = "www"
 	if (pContinentName == "EU") :
 		prefix = "eu"
-	
-	connection = httplib.HTTPConnection( prefix + ".wowarmory.com" )
-	# TODO: Transform names to encode special characters
-	connection.request( "GET", "/character-sheet.xml?r=%s&n=%s" % ( pRealmName, pCharacterName ) )
-	response = connection.getresponse()
-	if (response.status == 200 and response.reason == "OK"):
-		data = response.read()
-		return data
+
+	f = urllib2.urlopen( "http://%s.wowarmory.com/character-sheet.xml?r=%s&n=%s"
+			% ( prefix, pRealmName, pCharacterName ) )
+
+	return f.read()
+
+#	connection = httplib.HTTPConnection(
+#			"http://proxycs-toulouse.si.c-s.fr", "8080" )
+#	connection.connect()
+#	# TODO: Transform names to encode special characters
+#	connection.request( "GET", "%s.wowarmory.com/character-sheet.xml?r=%s&n=%s"
+#			% ( prefix, pRealmName, pCharacterName ) )
+#	response = connection.getresponse()
+#	if (response.status == 200 and response.reason == "OK"):
+#		data = response.read()
+#		return data
 
 if __name__ == "__main__":
 	app = QtGui.QApplication(sys.argv)
 
 	dialog = uic.loadUi("ArmoryOptionDialog.ui")
 
-	if (dialog.exec_() == QtGui.QDialog.Accepted):
+	if dialog.exec_() == QtGui.QDialog.Accepted :
+		proxySettings = {}
+		if dialog.mProxySettings.isChecked() :
+			proxySettings = { 'user' : dialog.mProxyUser.text(),
+							  'pass' : dialog.mProxyPassword.text(),
+							  'host' : dialog.mProxyHost.text(),
+							  'port' : dialog.mProxyPort.text().toInt()[0] }
 		characterPage = retrieveCharacterPage( dialog.mCharacterName.text(),
 											   dialog.mRealmName.text(),
-											   dialog.mContinent.currentText() )
+											   dialog.mContinent.currentText(),
+											   proxySettings )
 
 	displayer = QtGui.QTextEdit()
 	displayer.setReadOnly( True )
